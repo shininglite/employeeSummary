a52620c236d72bcdf670052b1e2251a3a5ce6b10
shininglite <vandeusent@gmail.com>
2020-03-29 13:13:33 -0500

    Write code to define and export the Employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+
+//create Employee class
+class Employee {
+  // constructor function takes three parameters: name, id, email
+  constructor (name, id, email) {
+  this.name = name;
+  this.id = id;
+  this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+
+// next two lines create var named employee, 
+// and trigger constructor function and methods in Employee class
+const employee = new Employee("John", 1001, "j@fakemail.com")
+console.log(Employee);
+
+//export Employee Class
+module.exports = Employee;
